6b08350a08d13201c8fb5323d791f0889dddfb15
Kenn Villegas <kenn.@phoenix.64.local>
2017-12-26 16:51:05 -0500

    ADDED AJAX XCODE PROJECT

---
+++ Ajax/Ajax/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ajax
+//
+//  Created by Kenn Villegas on 12/26/17.
+//  Copyright © 2017 Kenn Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ajax/Ajax/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Ajax
+//
+//  Created by Kenn Villegas on 12/26/17.
+//  Copyright © 2017 Kenn Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
